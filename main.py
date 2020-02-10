81383110317165c56f7a4288b222032ef48d12bb
Joseph Hahn <jmhahn@gmail.com>
2020-02-10 15:20:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
 print("Hello World")
-print("new line of text for Git testing!")
-#Developer: git push origin master --force
-#Also Developer: Oops, sorry, wrong chat.
-#Every other Developer: WHAT THE HECK ARE YOU DOING?
