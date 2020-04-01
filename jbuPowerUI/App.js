7ac16a25b109071ab561fe885d60726fc184b6d6
Joseph Hahn <jmhahn@gmail.com>
2020-04-01 17:16:07 -0500

    Update App.js

--- jbuPowerUI/App.js
+++ jbuPowerUI/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 
 import Header from "./components/Header";
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <View style={styles.screen}>
       <Header title="JBU Solar Power" />
       <Text style={styles.text}>Joseph Hahn</Text>
+      <Switch
+        style={styles.screen}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     flex: 1,
