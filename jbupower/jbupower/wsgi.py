e12c5e3dfc3de857e7ac8cc8bb3ddf8e3b438429
Joseph Hahn <jmhahn@gmail.com>
2020-04-01 17:18:30 -0500

    Update wsgi.py

--- jbupower/jbupower/wsgi.py
+++ jbupower/jbupower/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/ee3503-project/hotelhound')
+sys.path.append('/home/ubuntu/ee3503-project/hotelhound/hotelhound')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbupower.settings')
 
 application = get_wsgi_application()
+
+
